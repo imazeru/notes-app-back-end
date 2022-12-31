c88a5e999b8d311f811281b4688632b98f1dd8a4
Heru Kurniawan <imazeru08@gmail.com>
2022-12-31 16:06:54 +0700

    fix const handler.js

--- src/handler.js
+++ src/handler.js
@@ -66,10 +66,7 @@ const getNoteByIdHandler = (request,h) => {
 
 const editNoteByIdHandler = (request,h) => {
     const {id} = request.params;
-    //const {title,tags,body} = request.payload;
-    const title = 'baru';
-    const tags = 'tag_baru';
-    const body = 'OK';
+    const {title,tags,body} = request.payload;
     const updatedAt = new Date().toISOString;
     const index = notes.findIndex((note)=>note.id===id);
     if(index!==-1){
